1655d1ab8c8b87066e2ef62acf42bca4ac3cbcf5
Shah Faisal <s.faisal@hnr.com.pk>
2018-05-17 11:16:15 +0500

    Initial messsage

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+hbs.registerPartials(__dirname +'/views/partials');
+hbs.registerHelper('getCurrentYear' , ()=>{
+	return new Date().getFullYear();
+});
+app.use((req, res, next)=>{
+	var now = new Date().toString();
+	var log = `${now} : ${req.method} : ${req.url}` ;
+	console.log(log);
+	fs.appendFile('server.log' , log +'\n', (err) => {
+		if (err) {
+			connsole.log("Unable to write in file ")	;	
+		}
+	});
+	//console.log(`${now}`);
+	next();
+});
+
+app.use((req , res , next) =>{
+	res.render('maintenance.hbs');
+} );
+app.set('view_engine','hbs');
+app.use(express.static(__dirname +'/public'));
+app.get('/', (req, res)=>{
+	//res.send('<h1>Hello Express!</h1>');
+	//console.log('Test ');
+	res.send({
+		name: 'Faisal Hassan',
+		likes:[
+		'bike', 
+		'cycle'
+		]
+	});
+});
+
+
+app.get('/about',(req,res)=>{
+	res.render('about.hbs' , {
+		pageTitle:'About Page'
+	});
+});
+
+app.get('/bad' , (req, res)=> {
+
+	res.send({errorMessage:'bad Request'});
+});
+app.listen(3000 , ()=> {
+	console.log('Server is up on port 3000');
+});
